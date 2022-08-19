14136a9c6323f16623e67b37fbf0065fc71332fc
@jefferson_tavares <jefferson.tavares@yahoo.com.br>
2022-08-19 19:11:52 -0300

    Push files

---
+++ src/main/java/br/com/sp/fatec/projetolabbd5/Projetolabbd5Application.java
@@ -0,0 +1,13 @@
+package br.com.sp.fatec.projetolabbd5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetolabbd5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetolabbd5Application.class, args);
+	}
+
+}
